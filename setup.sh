e3dc2d8a6a9d8d72fd4ced6f40d6a7252d2614ff
edwinvelez <2299753+edwinvelez@users.noreply.github.com>
2025-07-05 05:35:37 -0400

    move paru installs to its own file

--- setup.sh
+++ setup.sh
@@ -144,36 +144,3 @@ usermod -aG docker edwinvelez
 
 echo "Enabling Docker socket"
 systemctl enable docker.socket
-
-echo "Installing paru"
-pacman -S --needed base-devel
-git clone https://aur.archlinux.org/paru.git
-cd paru
-makepkg -si --noconfirm --needed
-
-echo "Cleaning up paru build directory"
-cd ..
-rm -rf paru
-
-echo "Installing google-chrome"
-paru -S google-chrome --noconfirm --needed
-
-echo "Installing Visual Studio Code"
-paru -S visual-studio-code-bin --noconfirm --needed
-
-echo "Installing Zoom"
-paru -S zoom --noconfirm --needed
-
-echo "Installing Dropbox"
-paru -S \
-  dropbox \
-  python-gpgme \
-  --noconfirm --needed
-
-# https://wiki.archlinux.org/title/Dropbox#Prevent_automatic_updates
-rm -rf ~/.dropbox-dist
-install -dm0 ~/.dropbox-dist
-
-
-echo "Change user shell"
-usermod -s /usr/bin/zsh edwinvelez
